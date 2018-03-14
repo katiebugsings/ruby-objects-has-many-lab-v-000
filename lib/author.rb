6c0540cb633a1b17e1e871c20522cbffd1a74fab
Katie Cassell <kcassell944@gmail.com>
2018-03-14 19:07:27 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,6 +6,22 @@ def initialize(name)
   @posts = []
   end
 
+  def add_post
+    @posts << post
+    @@post_count += 1
+    post.author = self
+
+    def post_by_title(title)
+      post = Post.new(title)
+      @posts << post
+      @@post_count += 1
+      post.author = self
+    end
+
+    def self.post_count
+      @@post_count
+    end
+
   def posts
     @posts
   end
