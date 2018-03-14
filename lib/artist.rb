4f041820c6f0e2776b5b29f97fa48195a13fd857
Katie Cassell <kcassell944@gmail.com>
2018-03-14 18:08:10 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+  attr_accessor :name, :song
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @song << song
+end
+
+def songs
+  @songs
+  end
+end
