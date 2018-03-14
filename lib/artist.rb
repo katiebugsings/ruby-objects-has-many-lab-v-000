3e7558b4d698ac8c752d2c597c1b5194df9a5bc1
Katie Cassell <kcassell944@gmail.com>
2018-03-14 18:38:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,18 +9,19 @@ class Artist
 
   def add_song(song)
     @songs << song
+    @@song_count += 1
     song.artist = self
 end
 
 def add_song_by_name(name)
   song = Song.new(name)
   @songs << song
+  @@song_count += 1
   song.artist = self
 end
 
 def self.song_count
   @@song_count
-    song.artist = self += 1
 end
 
 def songs
